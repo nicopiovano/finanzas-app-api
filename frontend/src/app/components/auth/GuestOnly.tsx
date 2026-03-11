363e02e1b2fe2dee4c1641e6be66a8c036149dde
NicoPiovano <npiovano@sugit.com.ar>
2026-03-11 18:35:21 -0300

    Elimino frontend

--- frontend/src/app/components/auth/GuestOnly.tsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from 'react-router'
-import { useAuthStore } from '../../stores/auth'
-
-export function GuestOnly() {
-  const user = useAuthStore((s) => s.user)
-
-  if (user) return <Navigate to="/" replace />
-  return <Outlet />
-}
